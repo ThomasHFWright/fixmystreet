1614a5f1c6f7570ecf12d1044b5b04da10326920
Thomas Wright <thomas.h.f.wright@gmail.com>
2024-04-07 19:42:28 +0100

    added council name

--- fixmybsresort-cobrand/perllib/FixMyStreet/Cobrand/FixMyBSResort.pm
+++ fixmybsresort-cobrand/perllib/FixMyStreet/Cobrand/FixMyBSResort.pm
@@ -4,3 +4,5 @@ use base 'FixMyStreet::Cobrand::Default';
 sub is_council {
     return 1; # Return 1 to indicate this is a council cobrand
 }
+
+sub council_name { return 'Bom Sucesso Condomínio'; }
